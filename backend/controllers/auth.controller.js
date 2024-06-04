bb7f288a238c2e8d26370144fe939fe4ad3482c9
UnnatiSamaiyar <unnatis2910btechcse2025@kccitm.edu.in>
2024-06-05 01:24:34 +0530

    controller setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) => {
+    console.log("loginUser");
+}
+
+export const logout = (req, res) => {
+    console.log("logoutUser");
+}
+
+export const signup = (req, res) => {
+    console.log("signupUser");
+}
