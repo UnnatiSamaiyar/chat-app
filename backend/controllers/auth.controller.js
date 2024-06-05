b9e58a5c574a7c9be81ba71af364ccc6056f7ddb
UnnatiSamaiyar <unnatis2910btechcse2025@kccitm.edu.in>
2024-06-05 12:08:37 +0530

    DB Setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,17 @@
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const{fullName, username, password, confirmPassword, gender} = req.body;
+    } catch (error) {
+        
+    }
 }
 
 export const logout = (req, res) => {
+    res.send("logoutUser");
     console.log("logoutUser");
 }
 
 export const signup = (req, res) => {
+    res.send("signupUser")
     console.log("signupUser");
 }
